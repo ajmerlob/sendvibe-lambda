ac4b5421912111e38580cf744ee6735ec3e24afa
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-03 03:27:59 +0000

    it works, but I am not happy with the next step being another lambda function

--- handler.py
+++ handler.py
@@ -3,27 +3,39 @@ import google_auth_oauthlib.flow
 import google.oauth2.credentials
 import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('list_ids')
+
+def save_list(timestamp,page_token,result_list):
+  item = {}
+  item['timestamp'] = timestamp
+  item['page_token'] = page_token
+  item['ids'] = result_list
+  table.put_item(Item=item)
+
+
 API_SERVICE_NAME = 'gmail'
 API_VERSION = 'v1'
 
 
 def my_handler(event, context):
-    try:
-        del event['timestamp']
-    except:
-        pass
+    if event is None:
+        return "event was null"
+    if 'timestamp' not in event:
+        return "event had no timestamp"
+
+    timestamp = event['timestamp']
+    del event['timestamp']
+
     creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**event)
     gmail = googleapiclient.discovery.build(
       API_SERVICE_NAME, API_VERSION, credentials=creds)
    
     response = gmail.users().messages().list(userId='me').execute()
-    messages = []
     if 'nextPageToken' in response:
-        messages.extend(response['messages'])
+        save_list(timestamp,"initial",response['messages'])
     while 'nextPageToken' in response:
         page_token = response['nextPageToken']
         response = gmail.users().messages().list(userId='me', pageToken=page_token).execute()
-        messages.extend(response['messages'])
-
- 
-    return messages
+        save_list(timestamp,page_token,response['messages']) 
+    return "event success"
