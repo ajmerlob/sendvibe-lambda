81227d8ddbc7a9ed42551629e0f03dc13c1b6fe6
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-03 05:28:41 +0000

    updated test case in lambda to reflect actual schema

--- handler.py
+++ handler.py
@@ -11,36 +11,77 @@ table = dynamodb.Table('list_ids')
 
 def save_list(timestamp,page_token,result_list):
   item = {}
+  logging.error("starting save")
   item['timestamp'] = timestamp
   item['page_token'] = page_token
   item['ids'] = result_list
   table.put_item(Item=item)
+  logging.error("ending save")
 
 
 API_SERVICE_NAME = 'gmail'
 API_VERSION = 'v1'
 
 
-def my_handler(event, context):
+def per_record(event, context):
     if event is None:
         logging.error("event was null")
         return "event was null"
-    if 'timestamp' not in event:
-        logging.error("timestamp not in event" + str(event))
-        return "event had no timestamp"
 
-    timestamp = event['timestamp']
-    del event['timestamp']
+    if not isinstance(event, dict) or u'timestamp' not in event:
+        logging.error(type(event))
+        logging.error("timestamp not in event")
+        logging.error(event)
+        return  "not dict or missing timestamp"
+
+    logging.error("Appears to be working")
+    logging.error(event)
+
+    for key in event:
+        if 'S' in event[key]:
+            event[key] = event[key]['S']
+        if 'NULL' in event[key]:
+            event[key] = None
+
+    logging.error("Still appears to be working")
+    logging.error(event)
+
+    timestamp = event[u'timestamp']
+    del event[u'timestamp']
+
+    logging.error("Still appears to be working x2")
+    logging.error(event)
 
     creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**event)
     gmail = googleapiclient.discovery.build(
       API_SERVICE_NAME, API_VERSION, credentials=creds)
    
+    logging.error("Credentials Built")
     response = gmail.users().messages().list(userId='me').execute()
-    if 'nextPageToken' in response:
-        save_list(timestamp,"initial",response['messages'])
+    save_list(timestamp,"initial",response['messages'])
+
+    logging.error("Checking nextPageTokens")
     while 'nextPageToken' in response:
         page_token = response['nextPageToken']
         response = gmail.users().messages().list(userId='me', pageToken=page_token).execute()
         save_list(timestamp,page_token,response['messages']) 
-    return "event success"
+    logging.info("record success")
+    return "success"
+
+def my_handler(event,context):
+    if event is None:
+        logging.error("event was null")
+        return "event was null"
+
+    if u'Records' in event:
+        for record in event[u'Records']:
+            if u'dynamodb' in record and u'NewImage' in record[u'dynamodb']:
+                per_record(record[u'dynamodb'][u'NewImage'],context)
+            else:
+                logging.error("Missing dynamodb or NewImage")
+                logging.error(record)
+        return "got through 'em all"
+    else:
+        logging.error("No 'Records' field")
+        logging.error(event)
+        return "No 'Records' field" 
