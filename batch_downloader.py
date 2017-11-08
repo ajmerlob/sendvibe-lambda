ecb64f45d56ce6635de27e183b0d42ab2fa42647
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-08 03:43:18 +0000

    most of the way to full email extraction (non batch)

--- batch_downloader.py
+++ batch_downloader.py
@@ -2,6 +2,12 @@ import math
 import sys
 import boto3
 import json
+import googleapiclient.discovery
+import google_auth_oauthlib.flow
+import google.oauth2.credentials
+
+
+## Get the S3 Key so you can get the ids
 
 sqs = boto3.client('sqs')
 QueueUrl = "https://sqs.us-west-2.amazonaws.com/985724320380/email_ids_to_download"
@@ -12,9 +18,36 @@ timestamp = message['Messages'][0]['Body']
 sqs.delete_message(QueueUrl=QueueUrl,ReceiptHandle=message['Messages'][0]['ReceiptHandle'])
 print "deleted"
 
+## Trade the S3 key for the messages
+
 s3 = boto3.client('s3')
 obj =  s3.get_object(Bucket='email-id-lists',Key=timestamp)
-ids = json.loads(obj['Body'].read())
-for i in ids:
-  print i
-print len(ids)
+email_ids_list = json.loads(obj['Body'].read())
+
+## Now go build the credentials stored in dynamodb
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('tokens')
+
+dict_of_creds =  table.get_item(Key={"timestamp":timestamp})['Item']
+del dict_of_creds['timestamp']
+print dict_of_creds
+creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**dict_of_creds)
+
+## Now that you've got the ids - go get the messages
+import base64
+import email
+from apiclient import errors
+from apiclient.discovery import build
+
+service = build('gmail', 'v1',credentials=creds)
+
+count = 0
+for email_id in email_ids_list:
+  count += 1
+  msg_id = email_id['id']
+  message = service.users().messages().get(userId='me', id=msg_id).execute()
+
+  print 'Message snippet: %s' % message['snippet']
+
+  if count > 5:
+    break
