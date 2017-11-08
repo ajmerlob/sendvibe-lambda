093d8a1f7df48bb810679834d1e5796b9a7eb022
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-08 17:28:30 +0000

    starting to classify

--- handler.py
+++ handler.py
@@ -6,110 +6,117 @@ import logging
 import time
 import json
 
-logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('list_ids')
+class Handler:
+  def timestamp_mod(ts):
+    return ts.replace(":","-").replace(".","-").replace("+","-")
 
-s3 = boto3.client('s3')
-sqs = boto3.client('sqs')
-ecs = boto3.client('ecs')
-
-def save_list(timestamp,page_token,result_list):
-  item = {}
-  logging.error("starting save")
-  item['timestamp'] = timestamp
-  logging.error("ending save")
-  s3.put_object(Body=json.dumps(result_list),Bucket='email-id-lists',Key=timestamp)
-#  table.put_item(Item=item)
-  logging.error("s3 sent")
-  response = sqs.send_message(QueueUrl="https://us-west-2.queue.amazonaws.com/985724320380/email_ids_to_download",MessageBody=timestamp)
-  logging.error("sqs sent")
-  ecs.run_task(taskDefinition="get-emails-test-2:1")
-  logging.error("ending save2")
-
-API_SERVICE_NAME = 'gmail'
-API_VERSION = 'v1'
-
-
-def per_record(event, context):
-    if event is None:
-        logging.error("event was null")
-        return "event was null"
+  def __init__(self):
+    logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
     
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('list_ids')
+    
+    s3 = boto3.client('s3')
+    sqs = boto3.client('sqs')
+    ecs = boto3.client('ecs')
   
-    if not isinstance(event, dict) or u'timestamp' not in event:
-        logging.error(type(event))
-        logging.error("timestamp not in event")
-        logging.error(event)
-        return  "not dict or missing timestamp"
-
-    if event['token'] == 'ya29.Glv4BGZz0Cg1xK79AnuO2GoGii4Ig5HhK5XgVvIbbMf3mj4IHFjiKPC4Fw8ac-Lh32wxqPdtHDQcIPW1z335CB9nKempvTJaHu5275p790mS4nkhstQwPxqnvb8D':
-        logging.error("somehow it was BGZz0C")
-        return "was BGZz0C"
-
-    logging.error("Appears to be working")
-    logging.error(event)
-
-    for key in event:
-        if 'S' in event[key]:
-            event[key] = event[key]['S']
-        if 'NULL' in event[key]:
-            event[key] = None
-
-    logging.error("Still appears to be working")
-    logging.error(event)
-
-    timestamp = event[u'timestamp']
-    del event[u'timestamp']
-
-    logging.error("Still appears to be working x2")
-    logging.error(event)
-
-    creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**event)
-    gmail = googleapiclient.discovery.build(
-      API_SERVICE_NAME, API_VERSION, credentials=creds)
-   
-    logging.error("Credentials Built")
-    logging.error(creds.token)
-    response = gmail.users().messages().list(userId='me').execute()
-    messages = []
-    if 'messages' in response:
-        messages.extend(response['messages'])
-#    save_list(timestamp,"initial",response['messages'])
-
-    logging.error("Checking nextPageTokens")
-    while 'nextPageToken' in response:
-        logging.error("Executing nextPageToken")
-        page_token = response['nextPageToken']
-        response = gmail.users().messages().list(userId='me', pageToken=page_token).execute()
-        messages.extend(response['messages'])
-#        save_list(timestamp,page_token,response['messages']) 
-
-    save_list(timestamp,None,messages)
-    logging.error("record success")
-    return "success"
-
-def my_handler(event,context):
-    if event is None:
-        logging.error("event was null")
-        return "event was null"
-
-    if u'Records' in event:
-        for record in event[u'Records']:
-            if u'dynamodb' in record and u'NewImage' in record[u'dynamodb']:
-                try:
-                    per_record(record[u'dynamodb'][u'NewImage'],context)
-                except Exception, e:
-                    logging.error("Some Error Occurred")
-                    logging.error(record)
-                    logging.error(event)
-		    logging.error(e)
-            else:
-                logging.error("Missing dynamodb or NewImage")
-                logging.error(record)
-        return "Completed all records"
-    else:
-        logging.error("No 'Records' field")
-        logging.error(event)
-        return "No 'Records' field" 
+  def save_list(timestamp,page_token,result_list):
+    logging.error("starting save")
+    logging.error("ending save")
+  #  s3.put_object(Body=json.dumps(result_list),Bucket='email-id-lists',Key=timestamp)
+  #  table.put_item(Item=item)
+    queue = sqs.create_queue(QueueName=timestamp_mod(timestamp))
+    queueName = queue['QueueUrl']
+    sqs.send_message(QueueUrl=queueName,MessageBody=json.dumps(result_list))
+    logging.error("s3 sent")
+    response = sqs.send_message(QueueUrl="https://us-west-2.queue.amazonaws.com/985724320380/email_ids_to_download",MessageBody=timestamp)
+    logging.error("sqs sent")
+    ecs.run_task(taskDefinition="get-emails-test-2:1")
+    logging.error("ending save2")
+  
+  API_SERVICE_NAME = 'gmail'
+  API_VERSION = 'v1'
+  
+  
+  def per_record(event, context):
+      if event is None:
+          logging.error("event was null")
+          return "event was null"
+      
+    
+      if not isinstance(event, dict) or u'timestamp' not in event:
+          logging.error(type(event))
+          logging.error("timestamp not in event")
+          logging.error(event)
+          return  "not dict or missing timestamp"
+  
+      if event['token'] == 'ya29.Glv4BGZz0Cg1xK79AnuO2GoGii4Ig5HhK5XgVvIbbMf3mj4IHFjiKPC4Fw8ac-Lh32wxqPdtHDQcIPW1z335CB9nKempvTJaHu5275p790mS4nkhstQwPxqnvb8D':
+          logging.error("somehow it was BGZz0C")
+          return "was BGZz0C"
+  
+      logging.error("Appears to be working")
+      logging.error(event)
+  
+      for key in event:
+          if 'S' in event[key]:
+              event[key] = event[key]['S']
+          if 'NULL' in event[key]:
+              event[key] = None
+  
+      logging.error("Still appears to be working")
+      logging.error(event)
+  
+      timestamp = event[u'timestamp']
+      del event[u'timestamp']
+  
+      logging.error("Still appears to be working x2")
+      logging.error(event)
+  
+      creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**event)
+      gmail = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=creds)
+     
+      logging.error("Credentials Built")
+      logging.error(creds.token)
+      response = gmail.users().messages().list(userId='me').execute()
+      messages = []
+      if 'messages' in response:
+          messages.extend(response['messages'])
+  #    save_list(timestamp,"initial",response['messages'])
+  
+      logging.error("Checking nextPageTokens")
+      while 'nextPageToken' in response:
+          logging.error("Executing nextPageToken")
+          page_token = response['nextPageToken']
+          response = gmail.users().messages().list(userId='me', pageToken=page_token).execute()
+          messages.extend(response['messages'])
+  #        save_list(timestamp,page_token,response['messages']) 
+  
+      save_list(timestamp,None,messages)
+      logging.error("record success")
+      return "success"
+  
+  def my_handler(event,context):
+      if event is None:
+          logging.error("event was null")
+          return "event was null"
+  
+      if u'Records' in event:
+          for record in event[u'Records']:
+              if u'dynamodb' in record and u'NewImage' in record[u'dynamodb']:
+                  try:
+                      per_record(record[u'dynamodb'][u'NewImage'],context)
+                  except Exception, e:
+                      logging.error("Some Error Occurred")
+                      logging.error(record)
+                      logging.error(event)
+  		    logging.error(e)
+              else:
+                  logging.error("Missing dynamodb or NewImage")
+                  logging.error(record)
+          return "Completed all records"
+      else:
+          logging.error("No 'Records' field")
+          logging.error(event)
+          return "No 'Records' field" 
