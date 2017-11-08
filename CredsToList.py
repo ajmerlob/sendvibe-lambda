36a3e0fb8a49c709d71ddcde400298a2be47d4c1
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-08 18:12:35 +0000

    refactoring 1

--- CredsToList.py
+++ CredsToList.py
@@ -7,36 +7,36 @@ import time
 import json
 
 
-class Handler:
+class CredsToList:
+  API_SERVICE_NAME = 'gmail'
+  API_VERSION = 'v1'
+  
   def timestamp_mod(ts):
     return ts.replace(":","-").replace(".","-").replace("+","-")
 
   def __init__(self):
     logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
     
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('list_ids')
+    #dynamodb = boto3.resource('dynamodb')
+    #table = dynamodb.Table('list_ids')
     
     s3 = boto3.client('s3')
     sqs = boto3.client('sqs')
     ecs = boto3.client('ecs')
   
-  def save_list(timestamp,page_token,result_list):
+  def save_list(timestamp,page_token,result_list,queueName):
     logging.error("starting save")
     logging.error("ending save")
   #  s3.put_object(Body=json.dumps(result_list),Bucket='email-id-lists',Key=timestamp)
   #  table.put_item(Item=item)
-    queue = sqs.create_queue(QueueName=timestamp_mod(timestamp))
-    queueName = queue['QueueUrl']
+#    queue = sqs.create_queue(QueueName=timestamp_mod(timestamp))
+#    queueName = queue['QueueUrl']
     sqs.send_message(QueueUrl=queueName,MessageBody=json.dumps(result_list))
     logging.error("s3 sent")
     response = sqs.send_message(QueueUrl="https://us-west-2.queue.amazonaws.com/985724320380/email_ids_to_download",MessageBody=timestamp)
     logging.error("sqs sent")
     ecs.run_task(taskDefinition="get-emails-test-2:1")
     logging.error("ending save2")
-  
-  API_SERVICE_NAME = 'gmail'
-  API_VERSION = 'v1'
   
   
   def per_record(event, context):
@@ -79,6 +79,12 @@ class Handler:
      
       logging.error("Credentials Built")
       logging.error(creds.token)
+
+      ## Build the timestamp-titled queue to send id_lists
+      queue = sqs.create_queue(QueueName=timestamp_mod(timestamp))
+      queueName = queue['QueueUrl']
+
+      ## back to it
       response = gmail.users().messages().list(userId='me').execute()
       messages = []
       if 'messages' in response:
@@ -93,11 +99,11 @@ class Handler:
           messages.extend(response['messages'])
   #        save_list(timestamp,page_token,response['messages']) 
   
-      save_list(timestamp,None,messages)
+      save_list(timestamp,None,messages,queueName)
       logging.error("record success")
       return "success"
   
-  def my_handler(event,context):
+  def process_event(event,context):
       if event is None:
           logging.error("event was null")
           return "event was null"
@@ -111,7 +117,7 @@ class Handler:
                       logging.error("Some Error Occurred")
                       logging.error(record)
                       logging.error(event)
-  		    logging.error(e)
+  		      logging.error(e)
               else:
                   logging.error("Missing dynamodb or NewImage")
                   logging.error(record)
