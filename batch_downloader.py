9962e70604cf98b80a3cc771b2173bfb15c299a4
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-08 22:17:26 +0000

    refactor 2 + add message-reading loop

--- batch_downloader.py
+++ batch_downloader.py
@@ -16,79 +16,88 @@ from apiclient import errors
 from apiclient.discovery import build
 
 
-## Get the timestamp from the queue, which will act as the queue name to read the ids
+class Gmining:
+  
+  ## Get the timestamp from the queue, which will act as the queue name to read the ids
+  
+  def timestamp_mod(self,ts):
+    return ts.replace(":","-").replace(".","-").replace("+","-")
+ 
+  def build_creds(self):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('tokens')
+    dict_of_creds =  table.get_item(Key={"timestamp":self.timestamp})['Item']
+    del dict_of_creds['timestamp']
+    creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**dict_of_creds)
+    return creds
 
-def timestamp_mod(ts):
-  return ts.replace(":","-").replace(".","-").replace("+","-")
-
-sqs = boto3.client('sqs')
-s3 = boto3.client('s3')
-
-## Open SQS and grab the queue name (which is the modded timestamp)
-print 'getting timestamp'
-QueueUrlTimestamp = "https://sqs.us-west-2.amazonaws.com/985724320380/email_ids_to_download"
-timestamp_message = sqs.receive_message(QueueUrl=QueueUrlTimestamp,MaxNumberOfMessages=1,WaitTimeSeconds=20)
-timestamp = timestamp_message['Messages'][0]['Body']
-QueueUrlIds = "https://sqs.us-west-2.amazonaws.com/985724320380/" + timestamp_mod(timestamp)
-
-def build_creds(timestamp):
-  dynamodb = boto3.resource('dynamodb')
-  table = dynamodb.Table('tokens')
-  dict_of_creds =  table.get_item(Key={"timestamp":timestamp})['Item']
-  del dict_of_creds['timestamp']
-  creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**dict_of_creds)
-  return creds
-
-## Build resources for reading emails
-print 'building credentials'
-creds = build_creds(timestamp)
+  def process_id_list(self,id_list):
+    send = []
+    ## read the gmails (which are an API call to Gmail)
+    for id_dict in json.loads(id_list['Body']):
+  #    print email_id
+      msg_id = (id_dict['id'])
+      email = self.service.users().messages().get(userId='me', id=msg_id).execute()
     
-## Now that you've got the ids - go get the messages
-print 'accessing gmail'
-service = build('gmail', 'v1',credentials=creds)
-email_address = service.users().getProfile(userId='me').execute()['emailAddress']
+      email['email_address'] = self.email_address
+      send.append(email)
+    return send
 
-## Open up the queue with the ids
-print 'reading queue of ids'
-messages = sqs.receive_message(QueueUrl=QueueUrlIds,MaxNumberOfMessages=10,WaitTimeSeconds=20)
-print 'starting id reads'
-def process_id_list(id_list,email_address):
-  send = []
-  ## read the gmails (which are an API call to Gmail)
-  for id_dict in json.loads(id_list['Body']):
-#    print email_id
-    msg_id = (id_dict['id'])
-    email = service.users().messages().get(userId='me', id=msg_id).execute()
+  def send_to_s3(self,email_data):
+      print "s3 connection - writing {} emails".format(len(email_data))
+      obj = "\n".join([json.dumps(e) for e in email_data])
+      key ="e{}.{}".format(self.timestamp_mod(self.timestamp),time.time())
+      self.s3.put_object(Body=obj,Bucket='email-data-full',Key=key)
+
+  def __init__(self):
+    self.sqs = boto3.client('sqs')
+    self.s3 = boto3.client('s3')
   
-    email['email_address'] = email_address
-    send.append(email)
-  return send
-
-def send_to_s3(email_data,timestamp):
-    print "s3 connection"
-    obj = "\n".join([json.dumps(e) for e in email_data])
-    key ="e{}.{}".format(timestamp_mod(timestamp),time.time())
-    s3.put_object(Body=obj,Bucket='email-data-full',Key=key)
-   
-
-## Open up the messages, which are lists of 50 ids
-email_data = []
-count = 0
-for m in messages['Messages']:
-  count += 1
-  ## Buffer up the emails received
-  email_data.extend(process_id_list(m,email_address))
-  ## After each batch of messages, delete the message and sleep as needed
-  sqs.delete_message(QueueUrl=QueueUrlIds  ,ReceiptHandle=m['ReceiptHandle'])
-  ## After each 10 batches of messages, send buffer to S3
-  if count % 10 == 0:
-    send_to_s3(email_data,timestamp)
+    ## Open SQS and grab the queue name (which is the modded timestamp)
+    print 'getting timestamp'
+    self.QueueUrlTimestamp = "https://sqs.us-west-2.amazonaws.com/985724320380/email_ids_to_download"
+    timestamp_message = self.sqs.receive_message(QueueUrl=self.QueueUrlTimestamp,MaxNumberOfMessages=1,WaitTimeSeconds=20)
+    self.rh = timestamp_message['Messages'][0]['ReceiptHandle']
+    self.timestamp = timestamp_message['Messages'][0]['Body']
+    self.QueueUrlIds = "https://sqs.us-west-2.amazonaws.com/985724320380/" + self.timestamp_mod(self.timestamp)
+  
+    ## Build resources for reading emails
+    print 'building credentials'
+    creds = self.build_creds()
+      
+  ## Now that you've got the ids - go get the messages
+    print 'accessing gmail'
+    self.service = build('gmail', 'v1',credentials=creds)
+    self.email_address = self.service.users().getProfile(userId='me').execute()['emailAddress']
+  
+  def read_queue(self):
+    ## Open up the queue with the ids
+    print 'reading queue of ids'
+    list_of_id_lists = self.sqs.receive_message(QueueUrl=self.QueueUrlIds,MaxNumberOfMessages=10,WaitTimeSeconds=20)
+    print 'starting id reads'
+    
+    ## Open up the messages, which are lists of 50 ids
     email_data = []
+    count = 0
+    for id_list in list_of_id_lists['Messages']:
+      count += 1
+      ## Buffer up the emails received
+      email_data.extend(self.process_id_list(id_list))
+      ## After each batch of messages, delete the message and sleep as needed
+      self.sqs.delete_message(QueueUrl=self.QueueUrlIds  ,ReceiptHandle=id_list['ReceiptHandle'])
 
-if len(email_data)>0:
-  send_to_s3(email_data,timestamp)
+    ## After all the reading, send buffer to S3
+    self.send_to_s3(email_data)
 
-  
-sqs.delete_queue(QueueUrl=QueueUrlIds)
-sqs.delete_message(QueueUrl=QueueUrlTimestamp,ReceiptHandle=timestamp_message['Messages'][0]['ReceiptHandle'])
-print "deleted timestamp from queue and id-queue" 
+  def final_clean(self):
+    self.sqs.delete_queue(QueueUrl=self.QueueUrlIds)
+    self.sqs.delete_message(QueueUrl=self.QueueUrlTimestamp,ReceiptHandle=self.rh)
+    print "deleted timestamp from queue and id-queue" 
+    
+
+g = Gmining()    
+try:
+  while True:
+    g.read_queue()
+finally:
+  g.final_clean()
