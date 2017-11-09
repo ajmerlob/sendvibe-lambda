7016f1fa10f9d6deaab63994f06dddd685507d97
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-09 04:06:37 +0000

    hard-coding region data for boto

--- batch_downloader.py
+++ batch_downloader.py
@@ -24,12 +24,18 @@ class Gmining:
     return ts.replace(":","-").replace(".","-").replace("+","-")
  
   def build_creds(self):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb',region='us-west-2')
     table = dynamodb.Table('tokens')
     dict_of_creds =  table.get_item(Key={"timestamp":self.timestamp})['Item']
     del dict_of_creds['timestamp']
+    assert u'refresh_token' in dict_of_creds and dict_of_creds['refresh_token'] != u"", "refresh token was missing or blank"
     creds = google_auth_oauthlib.flow.credentials = google.oauth2.credentials.Credentials(**dict_of_creds)
     return creds
+
+  def refresh_auth(self):
+    print 'ISSUE: refreshing auth'
+    creds = self.build_creds()
+    self.service = build('gmail', 'v1',credentials=creds)
 
   def process_id_list(self,id_list):
     send = []
@@ -37,7 +43,12 @@ class Gmining:
     for id_dict in json.loads(id_list['Body']):
   #    print email_id
       msg_id = (id_dict['id'])
-      email = self.service.users().messages().get(userId='me', id=msg_id).execute()
+      try:
+        email = self.service.users().messages().get(userId='me', id=msg_id).execute()
+      except google.auth.exceptions.RefreshError:
+        self.refresh_auth()
+        email = self.service.users().messages().get(userId='me', id=msg_id).execute()
+        
     
       email['email_address'] = self.email_address
       send.append(email)
@@ -50,15 +61,15 @@ class Gmining:
       self.s3.put_object(Body=obj,Bucket='email-data-full',Key=key)
 
   def __init__(self):
-    self.sqs = boto3.client('sqs')
-    self.s3 = boto3.client('s3')
+    self.sqs = boto3.client('sqs',region='us-west-2')
+    self.s3 = boto3.client('s3',region='us-west-2')
   
     ## Open SQS and grab the queue name (which is the modded timestamp)
     print 'getting timestamp'
     self.QueueUrlTimestamp = "https://sqs.us-west-2.amazonaws.com/985724320380/email_ids_to_download"
     timestamp_message = self.sqs.receive_message(QueueUrl=self.QueueUrlTimestamp,MaxNumberOfMessages=1,WaitTimeSeconds=20)
-    self.sqs.delete_message(QueueUrl=self.QueueUrlTimestamp,ReceiptHandle=self.rh)
     self.rh = timestamp_message['Messages'][0]['ReceiptHandle']
+    self.sqs.delete_message(QueueUrl=self.QueueUrlTimestamp,ReceiptHandle=self.rh)
     self.timestamp = timestamp_message['Messages'][0]['Body']
     self.QueueUrlIds = "https://sqs.us-west-2.amazonaws.com/985724320380/" + self.timestamp_mod(self.timestamp)
   
@@ -69,13 +80,19 @@ class Gmining:
   ## Now that you've got the ids - go get the messages
     print 'accessing gmail'
     self.service = build('gmail', 'v1',credentials=creds)
-    self.email_address = self.service.users().getProfile(userId='me').execute()['emailAddress']
+    try:
+      self.email_address = self.service.users().getProfile(userId='me').execute()['emailAddress']
+    except google.auth.exceptions.RefreshError, e:
+      print e
+      raise Exception("{} was messed up".format(self.timestamp))
+      
+    print self.email_address
   
   def attempt_read_queue(self):
     for attempt in range(3):
       try:
         list_of_id_lists = self.sqs.receive_message(QueueUrl=self.QueueUrlIds,MaxNumberOfMessages=10,WaitTimeSeconds=20)
-        assert 'Messages' in list_of_id_lists, "Queue with ids was empty or returned nothing after 20 seconds - attempt {}".format(attempt)
+        assert 'Messages' in list_of_id_lists, "Queue with ids was empty or returned nothing after 20 seconds - attempt {}".format(attempt+1)
         return list_of_id_lists
       except AssertionError, ae:
         print ae
@@ -111,7 +128,7 @@ g = Gmining()
 try:
   while True:
     g.read_queue()
-except AssertionError:
-  pass
+except AssertionError, e:
+  print e
 finally:
   g.final_clean()
