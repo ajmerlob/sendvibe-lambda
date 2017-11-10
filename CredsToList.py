befd47034c639b756430abb3bfcd5c247446dba1
Ubuntu <ubuntu@ip-172-31-5-129.us-west-2.compute.internal>
2017-11-10 22:36:13 +0000

    seems to be working now

--- CredsToList.py
+++ CredsToList.py
@@ -40,7 +40,7 @@ class CredsToList:
         self.API_SERVICE_NAME, self.API_VERSION, credentials=creds)
      
       ## Shoot the email_address into the big-picture queue
-      response = self.sqs.send_message(QueueUrl="https://us-west-2.queue.amazonaws.com/985724320380/email_ids_to_download",MessageBody=email_address)
+      response = self.sqs.send_message(QueueUrl="https://us-west-2.queue.amazonaws.com/985724320380/email_ids_to_download",MessageBody=json.dumps((email_address,timestamp)))
 
       ## Build the timestamp-titled queue to send id_lists and the batch to extract the data from that queue
       queue = self.sqs.create_queue(QueueName=self.timestamp_mod(timestamp))
